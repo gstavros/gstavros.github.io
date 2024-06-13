3d848b895c5a566676efc49f2a0461bb874ac010
George Stavros <140923495+gstavros@users.noreply.github.com>
2024-06-13 15:49:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function sendMail() {
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    // Create an EmailJS service object
-    const service = 'service_7c4b521'; // Replace with your service ID
-    const template = 'template_3v37w4r'; // Replace with your template ID
+
+    const service = 'service_7c4b521'; 
+    const template = 'template_3v37w4r'; 
 
     // Send the email using EmailJS
     emailjs.send(service, template, {
@@ -22,18 +22,14 @@ function sendMail() {
     })
     .then((response) => {
         console.log('SUCCESS!', response.status, response.text);
-        // Display a success message to the user
         displayMessage("Message sent successfully!", "success"); 
-        // Clear the form after sending
         document.getElementById('contact-form').reset();
     }, (error) => {
         console.log('FAILED...', error);
-        // Display an error message to the user
         displayMessage("Oops! Something went wrong. Please try again later.", "error"); 
     });
 }
 
-// Function to display the success or error message
 function displayMessage(text, type) {
     const messageContainer = document.getElementById('message-container');
     const messageText = document.getElementById('message-text');
@@ -43,13 +39,11 @@ function displayMessage(text, type) {
     messageContainer.style.display = "block"; 
 }
 
-// Attach the sendMail function to the form's submit event
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
+    event.preventDefault();
     sendMail(); 
 });
 
-// Function to display the success or error message (Modified)
 function displayMessage(text, type) {
     const messageContainer = document.getElementById('message-container');
     const messageText = document.getElementById('message-text');
@@ -58,16 +52,14 @@ function displayMessage(text, type) {
     messageContainer.classList.add(type); 
     messageContainer.style.display = "block"; 
   
-    // Add event listener to OK button
     const okButton = document.getElementById('okButton');
     okButton.addEventListener('click', () => {
       messageContainer.style.display = "none";
       messageContainer.classList.remove(type); 
-      location.reload(); // Refresh the page
+      location.reload(); 
     });
   }
   
-  // Attach the sendMail function to the form's submit event
   document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent default form submission
     sendMail(); 
